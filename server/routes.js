36de007ea493b7069424570ab6a57f9c63f63e0f
Warren <warrendsteinacker@gmail.com>
2026-02-19 09:36:14 -0500

    makeing controllers

---
+++ server/routes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const router = express.Router()
+
+
+router.get('/1', )
+router.get('/2', )
+router.get('/3', )
+router.get('/4', )
+router.get('/5', )
+router.get('/6', )
+router.get('/7', )
+router.get('/8', )
+router.get('/9', )
+
+
+export default router
