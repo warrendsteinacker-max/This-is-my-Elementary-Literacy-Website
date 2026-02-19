de1857e6114a28fdd0dd2b4660b16a037158292a
Warren <warrendsteinacker@gmail.com>
2026-02-19 09:04:38 -0500

    making connection to ai api key

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import {GooleGenAI} from '@google/genai'
+
+
+app.use(express.json())
+
+app.use(cors({origin: '*'}))
+
+const genAI = GooleGenAI({
+    apiKey: process.env.AI_KEY
+});
+
+app.listen(3002, () => {
+    console.log('connected to server')
+})
+
+
+
+
 
 
