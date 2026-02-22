26c240f7db6b543aae1161bfd8f4f37f6e26ddb4
Warren <warrendsteinacker@gmail.com>
2026-02-21 20:43:57 -0500

    fixed problem

--- server/server.js
+++ server/server.js
@@ -1,22 +1,47 @@
+// import 'dotenv/config'
+// import express from 'express'
+// import cors from 'cors'
+// import routers from './routes.js'
+
+
+// const app = express()
+
+// app.use(cors({origin: '*'}))
+
+// app.use(express.json());
+
+// app.use('/', routers)
+
+// app.listen(3002, () => {
+//     console.log('connected to server')
+// })
+
+// // Keep your existing code, just add this:
+// export default app;
+
+
+
+
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import routers from './routes.js'
 
-
 const app = express()
 
 app.use(cors({origin: '*'}))
-
 app.use(express.json());
 
 app.use('/', routers)
 
-app.listen(3002, () => {
-    console.log('connected to server')
-})
+// ONLY run app.listen if we are NOT on Vercel
+// if (process.env.NODE_ENV !== 'production') {
+//     app.listen(3002, () => {
+//         console.log('Server running locally on port 3002')
+//     })
+// }
 
-// Keep your existing code, just add this:
+// Vercel needs this export to handle the request
 export default app;
 
 
