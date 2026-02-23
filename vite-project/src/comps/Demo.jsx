88f53ea43c0c9bcfb1ba51f3aa8342d812fd4f0f
Warren <warrendsteinacker@gmail.com>
2026-02-23 12:47:48 -0500

    creating search bar

--- vite-project/src/comps/Demo.jsx
+++ vite-project/src/comps/Demo.jsx
@@ -157,13 +157,13 @@ import {useState} from 'react'
 
 const seacrchB = () => {
 
-    const data = [{name: 'warren', dep: 'is doc'}, {name: 'stein', dep: 'is clerk'}, {name: 'steinacker', dep: 'is me'}]
+    const data = [{name: 'warren', dep: 'is doc', id: 1}, {name: 'stein', dep: 'is clerk', id: 2}, {name: 'steinacker', dep: 'is me', id: 3}]
 
   
   const [searched, setS] = useState(data)
 
   return(<>
         <input placeholder='search somthing' onChange={(e) => setS(e.target.value)}/>
-
+        {searched.map(() => <div key={searched.id} style={{display: 'flex', alignItems: 'center'}}><h3></h3><p></p></div>)}
         </>)
 }
