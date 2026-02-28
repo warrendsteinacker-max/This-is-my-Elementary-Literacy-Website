ddf3573cb356075fc10788b11ed44f1fcb45ed5b
Warren <warrendsteinacker@gmail.com>
2026-02-28 15:19:51 -0500

    making database connection

--- api/db.js
+++ api/db.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 
 
-// const connectDB = async() => {
+const connectDB = async() => {
 
-//     try{
-//         await mongoose.connect(process.env.MONGO_URI)
-//         console.log('connected')
-//     }
-//     catch(error){
-//         console.error(error.message)
-//         console.log('not connected') 
-//     }
-// }
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('connected')
+    }
+    catch(error){
+        console.error(error.message)
+        console.log('not connected') 
+    }
+}
 
 // In your connectDB.js
-const connectDB = async () => {
-    if (mongoose.connection.readyState >= 1) {
-        return; // Already connected
-    }
-    await mongoose.connect(process.env.MONGO_URI);
-};
+
+
+// const connectDB = async () => {
+//     if (mongoose.connection.readyState >= 1) {
+//         return; // Already connected
+//     }
+//     await mongoose.connect(process.env.MONGO_URI);
+// };
 
 export default connectDB
