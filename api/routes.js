205d8c77ffdc3e933395e3070c579c1b7a1cada6
Warren <warrendsteinacker@gmail.com>
2026-02-28 14:19:59 -0500

    making database connection

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { post1, post2, post3, post4, post5, post6, post7 } from './AIC.js'
-import { deleteP, editP, makeP } from './UserC.js'
+import { makeP } from './UserC.js'
 
 const router = express.Router()
 
-router.post('/delete', deleteP)
-router.post('/edit', editP)
+// router.post('/delete', deleteP)
+// router.post('/edit', editP)
 router.post('/make', makeP)
 router.post('/1', post1)
 router.post('/2', post2)
