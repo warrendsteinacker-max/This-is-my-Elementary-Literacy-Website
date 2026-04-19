905c027a819f44b66db1de17be9e0f425ebd75ac
Warren <warrendsteinacker@gmail.com>
2026-04-19 09:09:13 -0400

    styled the navigation for math page some more

--- vite-project/src/comps/NavForMtwo.jsx
+++ vite-project/src/comps/NavForMtwo.jsx
@@ -43,18 +43,18 @@ const NavForMtwo = () => {
                 <option onClick={FUNC}>Gallary walk</option>
             </select> */}
             
-            <Link to="/" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Home</Link>
-            <Link to="/A Growth Mindset" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Growth Mindset</Link>
+            <Link to="/" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "50px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center"}}>Home</Link>
+            <Link to="/A Growth Mindset" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "60px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Growth Mindset</Link>
             {/* <Link to="/Mind map using Google Drawing" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Mind map using Google Drawing</Link> */}
             {/* <Link to="/Building Mathematical Dispositions Posters" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>poster</Link> */}
             {/* <Link to="/Naming Standards and Diversifying Instruction" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}>Naming Standards and Diversifying Instruction</Link> */}
-            <Link to="/Naming Standards and Diversifying Instruction" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Diversifying Instruction</Link>
-            <Link to="/Virtual Gallery Walk: Rubrics Analyzed: Good, Bad, and Ugly" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Assessment</Link>
-            <Link to="/Auth" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Post Page</Link>
-            <Link to="/MDE Refs" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>MDE References</Link>
-            <Link to="/Lesson P" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Lesson Planning</Link>
-            <Link to="Tech" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black" }}>Technology</Link>
-            <Link to="FieldW" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black"}}>Fieldwork</Link>
+            <Link to="/Naming Standards and Diversifying Instruction" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Diversifying Instruction</Link>
+            <Link to="/Virtual Gallery Walk: Rubrics Analyzed: Good, Bad, and Ugly" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Assessment</Link>
+            <Link to="/Auth" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "50px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Post Page</Link>
+            <Link to="/MDE Refs" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>MDE References</Link>
+            <Link to="/Lesson P" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Lesson Planning</Link>
+            <Link to="Tech" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center" }}>Technology</Link>
+            <Link to="FieldW" style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold', border: "1px solid black", borderRadius: "5px", padding: "2px", backgroundColor: "black", width: "90px", height: "50px", display: "grid", justifyContent: "center", alignItems: "center"}}>Fieldwork</Link>
             </nav>
         </>
 
