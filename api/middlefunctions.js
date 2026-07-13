7701b45b8ce43a4a27873f4c541b6018ce566e68
warrendsteinacker-max <warrendsteinacker@gmail.com>
2026-07-13 17:19:39 -0400

    Improve token protection middleware
    Refactor protection function to handle token verification and errors.

--- api/middlefunctions.js
+++ api/middlefunctions.js
@@ -1,1 +1,45 @@
+import jwt from "jsonwebtoken"
 
+
+function protection(req, res) {
+
+const token = req.headers.authorization.split (" ")[1]
+
+  
+if(token){
+  
+try {
+
+  const decoded = jwt.verify(token, process.env.token)
+if(!decoded){
+
+
+  throw new Error("invalid token")
+
+  
+}
+
+  next()
+
+}
+catch(error){
+
+  if (error.message === "invalid token"){
+
+    res.status(401)
+
+}
+
+  else {
+res.status(500)
+
+  } 
+  
+}
+
+}
+}
+
+
+
+export default protection
